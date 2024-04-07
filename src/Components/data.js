a78f53a75b7020d85e99e1386a140361f2357339
Montassar <elsoulimontassar@gmail.com>
2024-04-07 21:43:22 +0100

    updating api

--- src/Components/data.js
+++ src/Components/data.js
@@ -182,3 +182,11 @@ const moviesData = [
 ];
 
 export default moviesData;
+/*
+"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
+
+"https://image.tmdb.org/t/p/w1280"
+
+"https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
+
+*/
